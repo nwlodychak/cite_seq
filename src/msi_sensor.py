8f4fe80f34c8adeffbc472e3fcfac42c1fba5d0d
nickwlodychak <swlodychak@gmail.com>
2025-03-31 18:44:37 -0400

    init commit of gmm_model

--- src/msi_sensor.py
+++ src/msi_sensor.py
@@ -1,5 +1,27 @@
+#!/usr/bin/env python3
+"""
+msi_sensor.py
+Created on March 30, 2025,
+Author: Nick Wlodychak
+
+This module provides functions for parsing single cell data into a format for msi-sensor from
+https://github.com/xjtu-omics/msisensor-rna
+
+msi-sensor requires gene expression data with a SampleID column, here we use the cell barcodes as the SampleID.
+
+- First we read in the scanpy dataframe and filter for empty cells.
+- Then we replace 0 values with extremely small values as SVM cannot tolerate 0s.
+- We verify model compatibility with the expression data, and then subset the genes to only those required for the model.
+- This is then written to a csv file that can be finally fed into msi-sensor.
+
+input file format: 10x expression data
+output file format: .csv containing MSI predictions
+
+:copyright: (c) 2025 by Nick Wlodychak
+"""
+
+import argparse
 import subprocess
-import scipy
 import pandas as pd
 import os
 from pathlib import Path
@@ -7,26 +29,26 @@ import scanpy as sc
 from sklearn.impute import KNNImputer
 from sklearn.impute import SimpleImputer
 import numpy as np
+import pickle
 
 
-def load_data_scanpy(expression_data) -> sc.AnnData:
+def load_data_scanpy(expression_data: Path) -> sc.AnnData:
     """
     This loads the gene expression data into scanpy and doe the normalization and QC steps.
     :param expression_data: Where the 10x expression data lives
     :return: patient_filtered: sc.AnnData opject with the filtered genes
     """
+
     print("Loading expression data...")
     rna_adata = sc.read_10x_mtx(expression_data)
-
-
 
     # patient id extraction
     rna_adata.obs['patient'] = rna_adata.obs_names.str.split('_').str[0]
     print("Unique patients:", rna_adata.obs['patient'].unique())
 
     # masking by patients
-    # patient = ["CID3586", "CID3838", "CDI3921", "CID3941", "CID3946", "CID3948", "CID3963"]
-    patient = ["CID3586"]
+    patient = ["CID3586", "CID3838", "CDI3921", "CID3941", "CID3946", "CID3948", "CID3963"]
+    # patient = ["CID3586"]
     patient_mask = rna_adata.obs['patient'].isin(patient)
     patient_filtered = rna_adata[patient_mask].copy()
 
@@ -40,24 +62,28 @@ def load_data_scanpy(expression_data) -> sc.AnnData:
     return patient_filtered
 
 
-def prepare_msisensor_input(adata, required_genes, outfile = "msisensor_input.csv", impute_method = 1) -> Path:
+def prepare_msisensor_input(rna_data: sc.AnnData, required_genes: set, outpath: Path) -> Path:
     """
     This function parses the 10x adata table and generates a msisensor input
-    :param adata: the adata table to be passed to MSI-sensor
-    :param outfile: The output csv file ready for MSI-sensor
+    :param rna_data: the adata table to be passed to MSI-sensor
+    :param required_genes: genes needed for the msi-sensor model
+    :param outpath: The output file location - ready for MSI-sensor
     :param impute_method: Method to impute missing data (0: mean replacement, 1: fill with 0, 2: KNN)
     :return: output_path: where the msisensor input file is
     """
+    outfile = "msisensor_input.csv"
+    output_path = Path(outpath, outfile)
+
     # convert to dataframe
-    df = adata.to_df().astype(np.float64)  # Float32 matches TCGA model
+    df = rna_data.to_df().astype(np.float64)
 
     # santitaztion
-    df = df.replace([np.inf, -np.inf], np.nan).fillna(1e-8)
-    df = df.replace(0, 1e-8)
-    df = df.filter(items=required_genes)
+    df = df.replace([np.inf, -np.inf], np.nan).fillna(1e-10)
+    df = df.replace(0, 1e-10)  # replace 0s for svm
+    df = df.filter(items = required_genes)
+
     # formatting
     df.index.name = 'SampleID'
-    output_path = Path(outpath, outfile)
     df.reset_index().to_csv(output_path, index = False, float_format = '%.16f')
 
     #
@@ -117,54 +143,64 @@ def prepare_msisensor_input(adata, required_genes, outfile = "msisensor_input.cs
     return output_path
 
 
-def run_msi_sensor(input, model, outfile = "msisensor_output.csv"):
+def run_msi_sensor(input: Path, outpath: Path, model: Path):
     """
     Runs the msi sensor pipeline on the input file
     :param input: a csv formatted file
     :param output_path: the msi sensor output directory
-    :return:
+    :return: Nothing
     """
+
+    outfile = "msisensor_output.csv"
     print("Initializing msi sensor pipeline...")
     try:
         output_path = Path(outpath, outfile)
         print("Running MSI-sensor pipeline")
-        # output = Path(output_path, "msi_predictions.csv")
         call = f"msisensor-rna detection -i {input} -o {output_path} -m {model} -d TRUE"
         subprocess.call(call, shell = True)
     except Exception as e:
-        print("An error occurred while processing msi sensor!")
+        print(f"An error occurred while processing msi sensor! {e}")
         exit(1)
     print(f"msi sensor pipeline complete!\n Data at: {output_path}")
 
 
-def verify_model_compatibility(model_path, adata):
+def verify_model_compatibility(model: Path, rna_data: sc.AnnData) -> set:
     """
     Check model compatibility with adata frame
-    :param model_path: path to the pickle mode
-    :param adata: adata read into memory
-    :return:
+    :param model: path to the pickle mode
+    :param rna_data: adata read into memory
+    :return: required_genes: list of genes needed for SVM
     """
-    import pickle
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
-    required_genes = model.feature_names_in_
-    missing = set(required_genes) - set(adata.var_names)
+
+    with open(model, 'rb') as f:
+        out_model = pickle.load(f)
+    required_genes = out_model.feature_names_in_
+    missing = set(required_genes) - set(adata.var_names) # set genes from both frames
     if missing:
         raise ValueError(f"Missing {len(missing)} model-required genes")
     else:
         print(f"Model compatible with {len(required_genes)} Genes")
     return required_genes
 
+
 if __name__ == "__main__":
+    params_dict = {
+        "rna_data_path": Path("data/SCP1039/expression"),
+        "outpath"      : Path("msisensor_pipeline"),
+        "model"        : Path("msisensor-rna/model/TCGA.MSIPopular.model.pkl")
+    }
+
+    # housekeeping
     os.chdir(Path(__file__).resolve().parent.parent)
-    rna_data_path = "data/SCP1039/expression"
-    outpath = "msisensor_pipeline"
-    model = "msisensor-rna/model/TCGA.MSIPopular.model.pkl"
+    os.makedirs(params_dict["outpath"], exist_ok = True)
 
-    os.makedirs(outpath, exist_ok = True)
-
-    adata = load_data_scanpy(rna_data_path)
-    required_genes = verify_model_compatibility(model, adata)
-    msi_input = prepare_msisensor_input(adata, required_genes)
-
-    run_msi_sensor(msi_input, model)
+    # main pipeline
+    adata = load_data_scanpy(expression_data = params_dict["rna_data_path"])
+    required_genes = verify_model_compatibility(model = params_dict["model"],
+                                                rna_data = adata)
+    msi_input = prepare_msisensor_input(rna_data = adata,
+                                        required_genes = required_genes,
+                                        outpath = params_dict["outpath"])
+    run_msi_sensor(input = msi_input,
+                   outpath = params_dict["outpath"],
+                   model = params_dict["model"])
